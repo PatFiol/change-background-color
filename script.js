421d4dcbe730c5f1d1dc26cdbf14158c50233fa0
Patricia Fiol <patricia.fiol@outlook.com>
2023-01-02 12:59:21 +0100

    add localStorage

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const mainButton = document.querySelector('#main-button')
 const buttons = document.querySelector('#button-container')
 const background = document.querySelector('.background')
 
-// (()=>{
+
 
 mainButton.addEventListener('click', showColors)
-
-// })();
 
 function showColors(e) {
 	e.preventDefault()
@@ -34,40 +32,56 @@ buttons.addEventListener('click', (e) => {
     switch (backgroundColor) {
       case ('red'):
         background.className = 'red';
+        localStorage.setItem('background color', 'red')
         break;
       case ('orange'):
         background.className = 'orange';
+        localStorage.setItem('background color', 'orange')
         break;
       case ('yellow'):
         background.className = 'yellow';
+        localStorage.setItem('background color', 'yellow')
         break;
       case ('light-green'):
         background.className = 'light-green';
+        localStorage.setItem('background color', 'light-green')
         break;
       case ('dark-green'):
         background.className = 'dark-green';
+        localStorage.setItem('background color', 'dark-green')
         break;
       case ('dark-blue'):
         background.className = 'dark-blue';
+        localStorage.setItem('background color', 'dark-blue')
         break;
       case ('light-blue'):
         background.className = 'light-blue';
+        localStorage.setItem('background color', 'light-blue')
         break;
       case ('purple'):
         background.className = 'purple';
+        localStorage.setItem('background color', 'purple')
         break;
       case ('pink'):
         background.className = 'pink';
-        break;
-      case ('white'):
-        background.className = 'white';
+        localStorage.setItem('background color', 'pink')
         break;
       case ('grey'):
         background.className = 'grey';
+        localStorage.setItem('background color', 'grey')
         break;
       case ('black'):
         background.className = 'black';
+        localStorage.setItem('background color', 'black')
         break;
       }
 
 })
+
+const color = localStorage.getItem('background color')
+
+function changeColor() {
+  return background.className = color
+}
+
+changeColor()
